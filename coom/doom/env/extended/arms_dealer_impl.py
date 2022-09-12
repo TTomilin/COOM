096bb4fa48fe5e891c7da35ba27988e7feb83c68
TTomilin <tristan.tomilin@hotmail.com>
2022-09-12 02:14:46 +0200

    Added optional acceleration

--- coom/doom/env/extended/arms_dealer_impl.py
+++ coom/doom/env/extended/arms_dealer_impl.py
@@ -10,6 +10,7 @@ class ArmsDealerImpl(ArmsDealer):
 
     def __init__(self, args: Namespace, task: str, task_id: int, num_tasks=1):
         self.reward_item_acquired = args.reward_item_acquired
+        self.add_speed = args.add_speed
         self.weapons_acquired = 0
         super().__init__(args, task, task_id, num_tasks, args.reward_delivery)
 
@@ -27,3 +28,17 @@ class ArmsDealerImpl(ArmsDealer):
     def clear_episode_statistics(self) -> None:
         self.weapons_acquired = 0
         self.arms_dealt = 0
+
+    def get_available_actions(self):
+        actions = []
+        speed = [[0.0], [1.0]]
+        m_forward = [[0.0], [1.0]]
+        t_left_right = [[0.0, 0.0], [0.0, 1.0], [1.0, 0.0]]
+        for m in m_forward:
+            for t in t_left_right:
+                if self.add_speed and m == [1.0]:
+                    for s in speed:
+                        actions.append(t + m + s)
+                else:
+                    actions.append(t + m)
+        return actions
