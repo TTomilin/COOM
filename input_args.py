62847e516d54505e2c16f48128c5c6d8bb520114
TTomilin <tristan.tomilin@hotmail.com>
2022-10-31 00:34:11 +0100

    Doubled weapon delivery reward

--- input_args.py
+++ input_args.py
@@ -130,7 +130,7 @@ def cl_parse_args(args=None):
     parser.add_argument('--reward_frame_survived', default=0.01, type=float, help='For surviving a frame')
     parser.add_argument('--reward_kill', default=5.0, type=float, help='For eliminating an enemy')
     parser.add_argument('--reward_item_acquired', default=15.0, type=float, help='For picking up weapons/health kits')
-    parser.add_argument('--reward_delivery', default=15.0, type=float, help='For delivering an item')
+    parser.add_argument('--reward_delivery', default=30.0, type=float, help='For delivering an item')
     parser.add_argument('--reward_scaler_traversal', default=1e-3, type=float,
                         help='Reward scaler for traversing the map')
 
