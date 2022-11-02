b0cf24184a789979c602135d908e9a392f818794
TTomilin <tristan.tomilin@hotmail.com>
2022-11-02 20:58:12 +0100

    New Scenario: Pitfall

---
+++ coom/env/scenario/pitfall.py
@@ -0,0 +1,49 @@
+from collections import deque
+
+from argparse import Namespace
+from typing import Dict, List
+from vizdoom import DEAD
+
+from coom.env.scenario.scenario import DoomEnv
+from coom.env.utils.wrappers import WrapperHolder, GameVariableProportionalRewardWrapper, BooleanVariableRewardWrapper
+
+
+class Pitfall(DoomEnv):
+
+    def __init__(self, args: Namespace, env: str, task_id: int, num_tasks: int = 1):
+        super().__init__(args, env, task_id, num_tasks)
+        self.reward_scaler_traversal = args.reward_scaler_traversal
+        self.penalty_death = args.penalty_death
+        self.frames = 0
+        self.total_dist = 0
+        self.current_dist = 0
+        self.distance_buffer = []
+
+    def store_statistics(self, game_var_buf: deque) -> None:
+        self.frames += 1
+        self.current_dist = game_var_buf[-1][0]
+        self.total_dist += self.current_dist
+        self.distance_buffer.append(self.current_dist)
+
+    def get_success(self) -> float:
+        return self.total_dist
+
+    def reward_wrappers(self) -> List[WrapperHolder]:
+        return [WrapperHolder(GameVariableProportionalRewardWrapper, self.reward_scaler_traversal, 0),
+                WrapperHolder(BooleanVariableRewardWrapper, self.penalty_death, DEAD)]
+
+    @property
+    def performance_upper_bound(self) -> float:
+        return 100000
+
+    @property
+    def performance_lower_bound(self) -> float:
+        return 0  # No distance traversed
+
+    def extra_statistics(self, mode: str = '') -> Dict[str, float]:
+        return {f'{mode}/distance': self.get_success(), f'{mode}/movement': self.get_success() / self.frames}
+
+    def clear_episode_statistics(self) -> None:
+        super().clear_episode_statistics()
+        self.distance_buffer.clear()
+        self.total_dist = 0
