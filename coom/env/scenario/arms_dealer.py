8b4fb80e5aae76cddbe279edda95f6a30abccffd
TTomilin <tristan.tomilin@hotmail.com>
2022-11-02 19:57:31 +0100

    Scenario description

--- coom/env/scenario/arms_dealer.py
+++ coom/env/scenario/arms_dealer.py
@@ -12,11 +12,21 @@ from coom.env.utils.wrappers import WrapperHolder, ConstantRewardWrapper, Moveme
 
 
 class ArmsDealer(DoomEnv):
+    """
+    In this scenario, the agent has to pick up weapons from the ground in a square shaped room, and deliver them to a
+    platform which appears at a random location after a weapon is obtained. The agent can carry multiple (the exact
+    amount is dependent on the weapon) weapons before making the delivery. A new weapon is spawned at a random location
+    whenever one is picked up, i.e., there is always a constant number of weapons on the ground. A new platform is
+    created for every weapon picked up. After a successful delivery, all the platforms disappear and the agent has to
+    start over by collecting weapons. The agent can turn left and right, move forward, and accelerate. The agent is
+    rewarded for both obtaining a weapon and delivering it. Additionally, a small reward is granted for how much the
+    agent moves in the environment. Finally, the agent is penalized for being idle.
+    """
 
     def __init__(self, args: Namespace, env: str, task_id: int, num_tasks: int = 1):
         super().__init__(args, env, task_id, num_tasks)
         self.reward_scaler_traversal = args.reward_scaler_traversal
-        self.penalty_frame_passed = args.penalty_frame_passed
+        self.penalty_passivity = args.penalty_passivity
         self.reward_item_acquired = args.reward_item_acquired
         self.reward_delivery = args.reward_delivery
         self.distance_buffer = []
@@ -36,7 +46,7 @@ class ArmsDealer(DoomEnv):
 
     def reward_wrappers(self) -> List[WrapperHolder]:
         return [
-            WrapperHolder(ConstantRewardWrapper, self.penalty_frame_passed, True),
+            WrapperHolder(ConstantRewardWrapper, self.penalty_passivity, True),
             WrapperHolder(UserVariableRewardWrapper, self.reward_item_acquired, GameVariable.USER1),
             WrapperHolder(UserVariableRewardWrapper, self.reward_delivery, GameVariable.USER2),
             WrapperHolder(MovementRewardWrapper),
@@ -44,7 +54,7 @@ class ArmsDealer(DoomEnv):
 
     @property
     def performance_upper_bound(self) -> float:
-        return 20.0  # TODO Figure this out
+        return 15.0  # Sufficient arms dealt
 
     @property
     def performance_lower_bound(self) -> float:
