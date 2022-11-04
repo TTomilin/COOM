cc3508f4df64011d9df83125f497967d5b1457a3
TTomilin <tristan.tomilin@hotmail.com>
2022-11-04 02:20:49 +0100

    Larger tiles (4->6)

--- coom/env/scenario/floor_is_lava.py
+++ coom/env/scenario/floor_is_lava.py
@@ -28,11 +28,11 @@ class FloorIsLava(DoomEnv):
 
     @property
     def performance_upper_bound(self) -> float:
-        return 2500.0  # Scenario length
+        return 2500  # Scenario length
 
     @property
     def performance_lower_bound(self) -> float:
-        return 750.0  # Frames until the lava scorches the player
+        return 800  # Frames until the lava scorches the player
 
     def clear_episode_statistics(self) -> None:
         super().clear_episode_statistics()
