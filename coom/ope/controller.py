0e6aa7298590ccdbb489bf784694c042cba0b883
TTomilin <tristan.tomilin@hotmail.com>
2022-12-10 21:10:38 +0100

    Create writer

--- coom/ope/controller.py
+++ coom/ope/controller.py
@@ -419,7 +419,7 @@ def main():
     args.timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     args.wandb_unique_id = f'{args.game}_{args.experiment_name}_{args.timestamp}'
     init_wandb(args)
-    # tb_writer = tf.summary.create_file_writer(os.path.join(ope_dir, 'logs', args.wandb_unique_id))
+    tb_writer = tf.summary.create_file_writer(os.path.join(ope_dir, 'logs', args.wandb_unique_id))
     # tb_writer.set_as_default()
 
     model = MDN_RNN(args.hidden_dim, args.z_dim, args.mixtures, args.predict_done)
