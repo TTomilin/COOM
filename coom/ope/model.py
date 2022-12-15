5082e21ffc07efa894368866b5f20920e9e5b4ad
TTomilin <tristan.tomilin@hotmail.com>
2022-12-15 18:22:48 +0100

    Import hack3

--- coom/ope/model.py
+++ coom/ope/model.py
@@ -15,7 +15,7 @@ from chainer.training import extensions
 from datetime import datetime
 from pathlib import Path
 
-sys.path.append('../')
+sys.path.insert(0, os.path.abspath('..'))
 try:
     from coom.utils.wandb_utils import init_wandb
 except:
