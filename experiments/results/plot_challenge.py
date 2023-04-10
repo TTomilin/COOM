74fe86b6ff55a4e3e12720cb0328a7508ab134b4
TTomilin <tristan.tomilin@hotmail.com>
2023-04-10 20:32:02 +0200

    Expanded plotting COC to multiple methods

--- experiments/results/plot_challenge.py
+++ experiments/results/plot_challenge.py
@@ -50,6 +50,9 @@ METRICS = {
     'default': 'kills',
 }
 
+PLOT_COLORS = ['#4C72B0', '#55A868', '#C44E52', '#8172B2', '#CCB974', '#64B5CD', '#777777', '#917113']
+LINE_STYLES = ['-', '--', ':', '-.']
+
 
 def main(args: argparse.Namespace) -> None:
     plt.style.use('seaborn')
@@ -59,7 +62,7 @@ def main(args: argparse.Namespace) -> None:
     metric = None
     n_rows = 2
     n_cols = int(np.ceil(n_envs / n_rows))
-    fig, ax = plt.subplots(n_rows, n_cols, sharex=True, figsize=(8, 4))
+    fig, ax = plt.subplots(n_rows, n_cols, sharex=True, figsize=(10, 4))
     max_steps = -np.inf
     task_length = args.task_length
     dof = len(seeds) - 1
@@ -68,33 +71,33 @@ def main(args: argparse.Namespace) -> None:
     for i, env in enumerate(SCENARIOS):
         row = i % n_cols
         col = i // n_cols
-        for sequence in sequences:
-            method = 'sac' if sequence == 'single' else 'packnet'
-            metric = args.metric if args.metric else METRICS[env]
-            seed_data = np.empty((len(seeds), task_length))
-            seed_data[:] = np.nan
-            for k, seed in enumerate(seeds):
-                path = os.path.join(os.getcwd(), sequence, method, f'seed_{seed}', f'{env}_{metric}.json')
-                if not os.path.exists(path):
-                    continue
-                with open(path, 'r') as f:
-                    task_start = 0 if sequence == 'single' else i * task_length
-                    data = json.load(f)[task_start: task_start + task_length]
-                steps = len(data)
-                max_steps = max(max_steps, steps)
-                seed_data[k, np.arange(steps)] = data
+        for j, sequence in enumerate(sequences):
+            for l, method in enumerate(args.methods):
+                metric = args.metric if args.metric else METRICS[env]
+                seed_data = np.empty((len(seeds), task_length))
+                seed_data[:] = np.nan
+                for k, seed in enumerate(seeds):
+                    path = os.path.join(os.getcwd(), sequence, method, f'seed_{seed}', f'{env}_{metric}.json')
+                    if not os.path.exists(path):
+                        continue
+                    with open(path, 'r') as f:
+                        task_start = i * task_length
+                        data = json.load(f)[task_start: task_start + task_length]
+                    steps = len(data)
+                    max_steps = max(max_steps, steps)
+                    seed_data[k, np.arange(steps)] = data
 
-            mean = np.nanmean(seed_data, axis=0)
-            mean = gaussian_filter1d(mean, sigma=2)
-            std = np.nanstd(seed_data, axis=0)
-            std = gaussian_filter1d(std, sigma=2)
+                mean = np.nanmean(seed_data, axis=0)
+                mean = gaussian_filter1d(mean, sigma=2)
+                std = np.nanstd(seed_data, axis=0)
+                std = gaussian_filter1d(std, sigma=2)
 
-            t_crit = np.abs(t.ppf(significance, dof))
-            ci = std * t_crit / np.sqrt(len(seeds))
+                t_crit = np.abs(t.ppf(significance, dof))
+                ci = std * t_crit / np.sqrt(len(seeds))
 
-            ax[col, row].plot(mean, label=TRANSLATIONS[sequence])
-            ax[col, row].tick_params(labelbottom=True)
-            ax[col, row].fill_between(np.arange(task_length), mean - ci, mean + ci, alpha=0.2)
+                ax[col, row].plot(mean, label=f'{TRANSLATIONS[method]} ({TRANSLATIONS[sequence]})', color=PLOT_COLORS[l], linestyle=LINE_STYLES[j])
+                ax[col, row].tick_params(labelbottom=True)
+                ax[col, row].fill_between(np.arange(task_length), mean - ci, mean + ci, alpha=0.2, color=PLOT_COLORS[l])
 
         ax[col, row].set_ylabel(TRANSLATIONS[metric])
         ax[col, row].set_title(TRANSLATIONS[env])
@@ -107,7 +110,7 @@ def main(args: argparse.Namespace) -> None:
     main_ax.xaxis.labelpad = 25
 
     handles, labels = ax[-1, -1].get_legend_handles_labels()
-    fig.legend(handles, labels, loc='lower center', bbox_to_anchor=(0.5, 0), ncol=3, fancybox=True, shadow=True)
+    fig.legend(handles, labels, loc='lower center', bbox_to_anchor=(0.5, 0), ncol=6, fancybox=True, shadow=True)
     fig.tight_layout()
     plt.savefig('plots/COC.png')
     plt.show()
@@ -115,7 +118,8 @@ def main(args: argparse.Namespace) -> None:
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument("--sequences", type=str, nargs="+", default=['COC', 'CO8'])
+    parser.add_argument("--sequences", type=str, nargs="+", default=['CO8', 'COC'])
+    parser.add_argument("--methods", type=str, nargs="+", choices=['packnet', 'vcl', 'mas', 'agem', 'l2', 'fine_tuning'])
     parser.add_argument("--metric", type=str, default=None, help="Name of the metric to plot")
     parser.add_argument("--confidence", type=float, default=0.9, help="Confidence interval")
     parser.add_argument("--task_length", type=int, default=200, help="Number of iterations x 1000 per task")
