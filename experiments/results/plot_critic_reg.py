55659dd8d2f6e56e119b3d49f8e71c676fe0b2ba
TTomilin <tristan.tomilin@hotmail.com>
2023-05-02 14:11:23 +0200

    Plotting critic regularization

---
+++ experiments/results/plot_critic_reg.py
@@ -0,0 +1,118 @@
+import argparse
+import json
+import numpy as np
+import os
+from matplotlib import pyplot as plt
+from scipy.ndimage import gaussian_filter1d
+from scipy.stats import t
+
+TRANSLATIONS = {
+    'packnet': 'PackNet',
+    'mas': 'MAS',
+    'agem': 'AGEM',
+    'l2': 'L2',
+    'vcl': 'VCL',
+    'fine_tuning': 'Fine-tuning',
+    'perfect_memory': 'Perfect Memory',
+
+    'pitfall': 'Pitfall',
+    'arms_dealer': 'Arms Dealer',
+    'hide_and_seek': 'Hide and Seek',
+    'floor_is_lava': 'Floor is Lava',
+    'chainsaw': 'Chainsaw',
+    'raise_the_roof': 'Raise the Roof',
+    'run_and_gun': 'Run and Gun',
+    'health_gathering': 'Health Gathering',
+
+    'reg_critic': 'Critic Regularization',
+    'no_reg_critic': 'No Critic Regularization'
+}
+
+SEQUENCES = {
+    'CD4': ['default', 'red', 'blue', 'shadows'],
+    'CD8': ['obstacles', 'green', 'resized', 'invulnerable', 'default', 'red', 'blue', 'shadows'],
+    'CO4': ['chainsaw', 'raise_the_roof', 'run_and_gun', 'health_gathering'],
+    'CO8': ['pitfall', 'arms_dealer', 'hide_and_seek', 'floor_is_lava', 'chainsaw', 'raise_the_roof', 'run_and_gun',
+            'health_gathering'],
+    'COC': ['pitfall', 'arms_dealer', 'hide_and_seek', 'floor_is_lava', 'chainsaw', 'raise_the_roof', 'run_and_gun',
+            'health_gathering'],
+}
+
+
+def main(cfg: argparse.Namespace) -> None:
+    plt.style.use('seaborn-deep')
+    plt.rcParams['axes.grid'] = True
+    seeds = args.seeds
+    regs = ['reg_critic', 'no_reg_critic']
+    sequence = cfg.sequence
+    fig, ax = plt.subplots(len(cfg.methods), 1, sharey='all', sharex='all', figsize=(12, 7))
+    env_names = [TRANSLATIONS[e] for e in SEQUENCES[sequence]]
+    n_envs = len(env_names)
+    max_steps = -np.inf
+    iterations = cfg.task_length * n_envs
+    n_seeds = len(seeds)
+    dof = n_seeds - 1
+    significance = (1 - cfg.confidence) / 2
+
+    for i, method in enumerate(cfg.methods):
+        for reg in regs:
+            seed_data = np.empty((n_envs, n_seeds, iterations))
+            seed_data[:] = np.nan
+            for e, env in enumerate(SEQUENCES[sequence]):
+                for k, seed in enumerate(seeds):
+                    path = f'{os.getcwd()}/{reg}/{sequence}/{method}/seed_{seed}/{env}_success.json'
+                    if not os.path.exists(path):
+                        print(f'Path {path} does not exist')
+                        continue
+                    with open(path, 'r') as f:
+                        data = json.load(f)
+                    data = data
+                    steps = len(data)
+                    max_steps = max(max_steps, steps)
+                    seed_data[e, k, np.arange(steps)] = data
+
+            mean = np.nanmean(seed_data, axis=(0, 1))
+            mean = gaussian_filter1d(mean, sigma=2)
+            std = np.nanstd(seed_data, axis=(0, 1))
+            std = gaussian_filter1d(std, sigma=2)
+
+            t_crit = np.abs(t.ppf(significance, dof))
+            ci = std * t_crit / np.sqrt(n_seeds * n_envs)
+
+            ax[i].plot(mean, label=TRANSLATIONS[reg])
+            ax[i].tick_params(labelbottom=True)
+            ax[i].fill_between(np.arange(iterations), mean - ci, mean + ci, alpha=0.25)
+
+        ax[i].set_ylabel('Average Success')
+        ax[i].set_title(TRANSLATIONS[method])
+
+    env_steps = max_steps // n_envs
+    env_name_locations = np.arange(0 + env_steps // 2, max_steps + env_steps // 2, env_steps)
+
+    ax2 = ax[0].twiny()
+    ax2.set_xlim(ax[0].get_xlim())
+    ax2.set_xticks(env_name_locations)
+    ax2.set_xticklabels(env_names)
+    ax2.tick_params(axis='both', which='both', length=0)
+
+    ax[-1].set_xlabel("Timesteps (K)", fontsize=13)
+    ax[-1].legend(loc='lower center', bbox_to_anchor=(0.5, -0.7), ncol=8, fancybox=True, shadow=True)
+    plt.tight_layout()
+    plt.savefig(f'plots/reg_critic.png')
+    plt.show()
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--sequence", type=str, default='CO8', choices=['CD4', 'CO4', 'CD8', 'CO8', 'COC'],
+                        help="Names of the task sequences")
+    parser.add_argument("--methods", type=str, default=['packnet', 'l2', 'agem'], help="Names of the methods")
+    parser.add_argument("--seeds", type=int, default=[1, 2, 3], help="")
+    parser.add_argument("--confidence", type=float, default=0.8, help="Confidence interval")
+    parser.add_argument("--task_length", type=int, default=200, help="Number of iterations x 1000 per task")
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
