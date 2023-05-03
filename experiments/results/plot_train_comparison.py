408f9e2295bc7b89ce208c9d0d4da440a08293d4
TTomilin <tristan.tomilin@hotmail.com>
2023-05-03 16:10:00 +0200

    Improved plotting COC training comparison per env

--- experiments/results/plot_train_comparison.py
+++ experiments/results/plot_train_comparison.py
@@ -55,14 +55,14 @@ LINE_STYLES = ['-', '--', ':', '-.']
 
 
 def main(args: argparse.Namespace) -> None:
-    plt.style.use('seaborn')
+    plt.style.use('seaborn-paper')
     seeds = ['1', '2', '3']
     sequences = args.sequences
     n_envs = len(SCENARIOS)
     metric = None
     n_rows = 2
     n_cols = int(np.ceil(n_envs / n_rows))
-    fig, ax = plt.subplots(n_rows, n_cols, sharex=True, figsize=(10, 4))
+    fig, ax = plt.subplots(n_rows, n_cols, sharex='all', figsize=(10, 4))
     max_steps = -np.inf
     task_length = args.task_length
     dof = len(seeds) - 1
@@ -97,11 +97,12 @@ def main(args: argparse.Namespace) -> None:
 
                 ax[col, row].plot(mean, label=f'{TRANSLATIONS[method]} ({TRANSLATIONS[sequence]})', color=PLOT_COLORS[l], linestyle=LINE_STYLES[j])
                 ax[col, row].tick_params(labelbottom=True)
-                ax[col, row].fill_between(np.arange(task_length), mean - ci, mean + ci, alpha=0.2, color=PLOT_COLORS[l])
+                ax[col, row].fill_between(np.arange(task_length), mean - ci, mean + ci, alpha=0.3, color=PLOT_COLORS[l])
 
         ax[col, row].set_ylabel(TRANSLATIONS[metric])
         ax[col, row].set_title(TRANSLATIONS[env])
-        ax[col, row].yaxis.set_label_coords(-0.25, 0.5)
+        ax[col, row].yaxis.set_label_coords(-0.2, 0.5)
+        ax[col, row].ticklabel_format(axis='y', style='sci', scilimits=(0, 5))
 
     main_ax = fig.add_subplot(1, 1, 1, frameon=False)
     main_ax.get_xaxis().set_ticks([])
@@ -110,9 +111,9 @@ def main(args: argparse.Namespace) -> None:
     main_ax.xaxis.labelpad = 25
 
     handles, labels = ax[-1, -1].get_legend_handles_labels()
-    fig.legend(handles, labels, loc='lower center', bbox_to_anchor=(0.5, 0), ncol=6, fancybox=True, shadow=True)
+    fig.legend(handles, labels, loc='lower center', bbox_to_anchor=(0.5, 0.02), ncol=6, fancybox=True, shadow=True)
     fig.tight_layout()
-    plt.savefig('plots/train_comparison.png')
+    plt.savefig('plots/COC/train_per_env.png')
     plt.show()
 
 
@@ -121,7 +122,7 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("--sequences", type=str, nargs="+", default=['CO8', 'COC'])
     parser.add_argument("--methods", type=str, nargs="+", choices=['packnet', 'vcl', 'mas', 'agem', 'l2', 'fine_tuning'])
     parser.add_argument("--metric", type=str, default=None, help="Name of the metric to plot")
-    parser.add_argument("--confidence", type=float, default=0.9, help="Confidence interval")
+    parser.add_argument("--confidence", type=float, default=0.95, help="Confidence interval")
     parser.add_argument("--task_length", type=int, default=200, help="Number of iterations x 1000 per task")
     return parser.parse_args()
 
