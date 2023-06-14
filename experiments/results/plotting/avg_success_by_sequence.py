a9f019fd182d73910c1da07cbf3656875ea1bc61
TTomilin <tristan.tomilin@hotmail.com>
2023-06-14 14:15:10 +0200

    Fixed plotting both single and multiple sequences

--- experiments/results/plotting/avg_success_by_sequence.py
+++ experiments/results/plotting/avg_success_by_sequence.py
@@ -9,16 +9,18 @@ def main(cfg: argparse.Namespace) -> None:
     seeds, metric, sequences = cfg.seeds, cfg.metric, cfg.sequences
     n_sequences, n_seeds = len(sequences), len(seeds)
     figsize = (10, 6) if n_sequences > 1 else (9, 3)
-    fig, ax = plt.subplots(n_sequences, 1, sharey='all', sharex='all', figsize=figsize)
+    fig, axes = plt.subplots(n_sequences, 1, sharey='all', sharex='all', figsize=figsize)
     colors = COLORS[cfg.sequence]
+    log_interval = LOG_INTERVAL if n_sequences == 1 else 1
+    assert len(sequences) > 0, "No sequences provided"
 
     for i, sequence in enumerate(sequences):
-        ax = ax if n_sequences == 1 else ax[i]
+        ax = axes if n_sequences == 1 else axes[i]
         envs = SEQUENCES[sequence]
         n_envs = len(envs)
         methods = METHODS if n_envs == 4 else METHODS[:-1]
-        iterations = cfg.task_length * n_envs * LOG_INTERVAL
-        n_data_points = int(iterations / 1000)
+        iterations = cfg.task_length * n_envs * log_interval
+        n_data_points = cfg.task_length * n_envs
         for j, method in enumerate(methods):
             data = get_data_per_env(envs, n_data_points, method, metric, seeds, sequence)
             plot_curve(ax, cfg.confidence, colors[j], TRANSLATIONS[method], iterations, data, n_seeds * n_envs,
@@ -31,15 +33,18 @@ def main(cfg: argparse.Namespace) -> None:
             ax.set_title(sequence, fontsize=14)
         add_task_labels(ax, envs, iterations, n_envs, fontsize=9)
 
-    # bottom_ax = ax if n_sequences == 1 else ax[-1]
-    # bottom_ax.set_xlabel("Timesteps (K)", fontsize=10)
-    # plt.ticklabel_format(axis='x', style='sci', scilimits=(1, 1))
-    ax.legend(ncol=2)
+    if n_sequences > 1:
+        bottom_ax = axes if n_sequences == 1 else axes[-1]
+        bottom_ax.set_xlabel("Timesteps (K)", fontsize=10)
+        bottom_ax.legend(loc='lower center', bbox_to_anchor=(0.5, -0.45), ncol=len(methods), fancybox=True, shadow=True)
+    else:
+        ax.legend(ncol=2)
+    plt.tight_layout()
     folder = 'success'
     os.makedirs(folder, exist_ok=True)
-    plot_name = f'{folder}/{"_".join(sequences)}'
-    plt.tight_layout()
-    plt.savefig(f'plots/{plot_name}.pdf')
+    plot_name = f'plots/{folder}/{"_".join(sequences)}.png'
+    print(f'Saving plot to {plot_name}')
+    plt.savefig(plot_name)
     plt.show()
 
 if __name__ == "__main__":
