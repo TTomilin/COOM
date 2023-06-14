035d58b1cf5170b78aed1bd7575405a851cff5c6
TTomilin <tristan.tomilin@hotmail.com>
2023-06-14 19:35:33 +0200

    Improved plotting scripts

--- experiments/results/plotting/performance_by_methods.py
+++ experiments/results/plotting/performance_by_methods.py
@@ -11,15 +11,15 @@ def main(cfg: argparse.Namespace) -> None:
     n_methods = len(methods)
     figsize = (12, 14) if n_methods > 1 else (11, 2.5)
     fig, ax = plt.subplots(len(methods), 1, sharey='all', sharex='all', figsize=figsize)
-    iterations = cfg.task_length * n_envs * LOG_INTERVAL
-    n_data_points = int(iterations / 1000)
+    n_data_points = cfg.task_length * n_envs
+    iterations = n_data_points * LOG_INTERVAL
 
     for i, method in enumerate(methods):
         cur_ax = ax if n_methods == 1 else ax[i]
         for j, env in enumerate(envs):
             data = get_data(env, n_data_points, method, metric, seeds, sequence)
-            plot_curve(cur_ax, cfg.confidence, colors[j], TRANSLATIONS[env], iterations, data, n_seeds)
-
+            plot_curve(cur_ax, cfg.confidence, colors[j], TRANSLATIONS[env], iterations, data, n_seeds,
+                       interval=LOG_INTERVAL)
         if n_methods > 1:
             cur_ax.set_title(TRANSLATIONS[method], fontsize=12)
         cur_ax.set_ylabel(TRANSLATIONS[metric])
@@ -32,11 +32,12 @@ def main(cfg: argparse.Namespace) -> None:
     n_cols = n_envs if n_envs == 4 else n_envs // 2 if n_methods > 1 else 1
     method = f'_{methods[0]}' if n_methods == 1 else ''
     plot_name = f'method/{sequence}_{metric}{method}'
-    vertical_anchor = -1.1 if n_methods > 1 else -0.1
+    vertical_anchor = -0.8 if n_methods > 1 else -0.1
     bottom_adjust = 0 if n_methods > 1 else 0
     loc = 'lower center' if n_methods > 1 else 'lower right'
+    horizontal_anchor = 1.225 if n_methods == 1 else 0.5
     plot_and_save(ax=bottom_ax, plot_name=plot_name, n_col=n_cols, vertical_anchor=vertical_anchor,
-                  bottom_adjust=bottom_adjust, loc=loc, horizontal_anchor=1.225)
+                  bottom_adjust=bottom_adjust, loc=loc, horizontal_anchor=horizontal_anchor, add_xlabel=n_methods > 1)
 
 
 if __name__ == "__main__":
--- experiments/results/plotting/transfer_all.py
+++ experiments/results/plotting/transfer_all.py
@@ -10,15 +10,16 @@ def main(cfg: argparse.Namespace) -> None:
     seeds, sequence, methods, metric = cfg.seeds, cfg.sequence, cfg.methods, cfg.metric
     envs = SEQUENCES[sequence]
     n_envs = len(envs)
-    if n_envs == 8:
-        methods = [method for method in methods if method != 'perfect_memory']
+    if methods is None:
+        methods = METHODS if n_envs == 4 else METHODS[:-1]
     n_methods = len(methods)
-    figsize = (9, 4) if n_methods > 1 else (10, 2)
+    figsize = (12, 12) if n_methods > 1 else (10, 2)
     fig, ax = plt.subplots(n_methods, 1, sharex='all', sharey='all', figsize=figsize)
     task_length = cfg.task_length
-    iterations = task_length * n_envs * LOG_INTERVAL
+    n_data_points = task_length * n_envs
+    iterations = n_data_points * LOG_INTERVAL
     baseline = get_baseline_data(sequence, seeds, task_length, cfg.metric)
-    baseline = gaussian_filter1d(baseline, sigma=2)
+    baseline = gaussian_filter1d(baseline, sigma=KERNEL_SIGMA)
 
     for i, method in enumerate(methods):
         cur_ax = ax if n_methods == 1 else ax[i]
@@ -36,15 +37,13 @@ def main(cfg: argparse.Namespace) -> None:
                     seed_data[k, np.arange(task_start, task_start + steps)] = data
 
         mean = np.nanmean(seed_data, axis=0)
-        mean = gaussian_filter1d(mean, sigma=2)
+        mean = gaussian_filter1d(mean, sigma=KERNEL_SIGMA)
         x = np.arange(0, iterations, LOG_INTERVAL)
         cur_ax.plot(x, mean, label=method, color=COLORS[i])
         cur_ax.plot(x, baseline, label='sac', color=COLOR_SAC)
-        cur_ax.ticklabel_format(style='sci', axis='x', scilimits=(0, 2))
         cur_ax.fill_between(x, mean, baseline, where=(mean < baseline), alpha=0.3, color=COLOR_SAC, interpolate=True)
         cur_ax.fill_between(x, mean, baseline, where=(mean >= baseline), alpha=0.3, color=COLORS[i], interpolate=True)
 
-        # ax[i].set_ylabel('Current Task Success', fontsize=11)
         if n_methods > 1:
             cur_ax.set_title(TRANSLATIONS[method], fontsize=13)
         cur_ax.set_xlim([0, iterations])
@@ -53,9 +52,11 @@ def main(cfg: argparse.Namespace) -> None:
 
     top_ax = ax if n_methods == 1 else ax[0]
     bottom_ax = ax if n_methods == 1 else ax[-1]
-    add_task_labels(top_ax, envs, iterations, n_envs, fontsize=10)
-    main_ax = add_main_ax(fig)
-    main_ax.set_ylabel('Current Task Success', fontsize=10, labelpad=25)
+    bottom_ax.ticklabel_format(style='sci', axis='x', scilimits=(0, 2))
+    bottom_ax.tick_params(labelbottom=True)
+    add_task_labels(top_ax, envs, iterations, n_envs, fontsize=12)
+    main_ax = add_main_ax(fig, fontsize=13)
+    main_ax.set_ylabel('Current Task Success', fontsize=13, labelpad=25)
 
     handles, labels = ax.get_legend_handles_labels() if n_methods == 1 else get_handles_and_labels(ax)
     sac_idx = labels.index('sac')
@@ -70,9 +71,12 @@ def main(cfg: argparse.Namespace) -> None:
         n_cols = n_envs if n_envs == 4 else n_methods + 1
         bottom_ax.legend(handles, labels, loc='lower center', bbox_to_anchor=(0.5, anchor), ncol=n_cols, fancybox=True,
                          shadow=True)
-    bottom_adjust = -0.25 if n_methods > 1 else 0
+    bottom_adjust = -0.05 if n_methods > 1 else 0
     plt.tight_layout(rect=[0, bottom_adjust, 1, 1])
-    plt.savefig(f'plots/transfer/{sequence}_{"_".join(methods)}.pdf')
+    file_path = 'plots/transfer'
+    os.makedirs(file_path, exist_ok=True)
+    file_name = f'{file_path}/{sequence}.png' if not cfg.methods else f'{file_path}/{sequence}_{"_".join(methods)}.png'
+    plt.savefig(file_name)
     plt.show()
 
 
