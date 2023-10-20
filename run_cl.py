62a29313e67f08d49222ab613971757628c026e4
TTomilin <tristan.tomilin@hotmail.com>
2023-10-21 01:17:47 +0200

    Fixed import

--- run_cl.py
+++ run_cl.py
@@ -5,7 +5,7 @@ from enum import Enum
 from pathlib import Path
 
 from cl.methods.agem import AGEM_SAC
-from cl.methods.episodic_replay import ClonExSAC
+from cl.methods.clonex import ClonExSAC
 from cl.methods.ewc import EWC_SAC
 from cl.methods.l2 import L2_SAC
 from cl.methods.mas import MAS_SAC
